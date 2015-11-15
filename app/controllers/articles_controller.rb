0a1cab22dbf3dd83e6a9d01ec843d53d33ffd36e
AutoBartek <bartekwyskocki@gmail.com>
2015-11-15 18:30:00 +0100

    Created Article
    Created Article Model, View and Controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,47 @@
 class ArticlesController < ApplicationController
+	before_action :get_article_by_id, only: [:edit, :update, :show, :destroy]
+	def get_article_by_id
+		@article = Article.find(params[:id])
+	end
+
+
+	def new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		@article.save
+		redirect_to @article
+	end
+
+	def edit
+	end
+
+	def update
+		if @article.update_attributes(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
+	def show
+	end
+
+	def index
+		@articles = Article.all
+	end
+
+	def destroy
+		@article.destroy
+		redirect_to articles_path
+	end
+
+
+	private
+		def article_params
+			params.require(:article).permit(:title, :text)
+		end
+
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,12 @@
 class Article < ActiveRecord::Base
+	validates :title,
+		presence: true,
+		uniqueness: true,
+		length: { in: 5..50 }
+
+	validates :text,
+		presence: true,
+		length: { in: 10..1000 }
+
+
 end
